008411659f3a312eb81b98af33bc88bf932d55d0
zhaoxincheng <zhaoxincheng@didiglobal.com>
2024-04-02 14:07:52 +0800

    init

---
+++ app/arm64-v8a/frida.js
@@ -0,0 +1,9 @@
+ console.log("123456789")
+Java.perform(function () {
+    console.log("dghfhjsafdhjasfdhajs")
+    let MainActivity = Java.use("com.example.testga.MainActivity");
+    MainActivity["sss"].implementation = function (view) {
+        this.ssssss.value = "testfrida1110"
+        this["sss"](view);
+    };
+})
